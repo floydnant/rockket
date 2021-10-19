906ca023792b32ec307530333f154c58fa0d9ff2
dein-ding <floyd.h@gmx.de>
2021-10-19 18:02:36 +0200

    fixed some edit-menu shit and remade some styling

--- src/app/edit-menu/edit-menu.component.ts
+++ src/app/edit-menu/edit-menu.component.ts
@@ -56,7 +56,7 @@ export class EditMenuComponent implements OnInit {
     }
 
     close(btnRes: returnInterface['responseStatus']) {
-        this.addLink();
+        if (this.type == 'Task') this.addLink();
         this.modalService.close('edit-menu');
         this.isOpen = false;
         this.editMenuService.return({ updatedProps: getCopyOf(this.dataEdit), responseStatus: btnRes });
