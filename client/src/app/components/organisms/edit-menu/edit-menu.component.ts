fbd923143746223174078966a52736b34cc29abf
dein-ding <floyd.h@gmx.de>
2022-03-05 22:20:29 +0100

    improved 'abandone changes' prompt

--- client/src/app/components/organisms/edit-menu/edit-menu.component.ts
+++ client/src/app/components/organisms/edit-menu/edit-menu.component.ts
@@ -63,11 +63,12 @@ export class EditMenuComponent implements OnInit {
             const confirmationResponse = await this.dialogService
                 .confirm({
                     title: 'Abandone changes?',
-                    text: 'You already made some changes, want to abandone them?',
-                    buttons: ['Abandone', 'Cancel'],
+                    text: "You already made some changes, don't you want to save them?",
+                    buttons: ['Abandone', 'Keep editing', 'Save'],
                 })
                 .catch(err => err);
-            if (confirmationResponse == 'Cancel') return;
+            if (confirmationResponse == 'Keep editing') return;
+            if (confirmationResponse == 'Save') responseStatus = 'OK';
         }
 
         this.isOpen = false;
