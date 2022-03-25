6cb00dc3e4dbd11dcde371a3615146e8ffa93ed1
dein-ding <floyd.h@gmx.de>
2022-03-25 22:55:41 +0100

    slight app component refactoring

--- client/src/app/components/app.component.ts
+++ client/src/app/components/app.component.ts
@@ -4,7 +4,7 @@ import { AppData, AppState, AppDataActions } from '../reducers';
 import { AppDataService } from '../reducers/appData/appData.service';
 
 import { sortCompletedTasks, Task } from '../shared/task.model';
-import { TaskList, countOpenTasks } from '../shared/taskList.model';
+import { TaskList, countTasks } from '../shared/taskList.model';
 import { getCopyOf, isTouchDevice, moveToMacroQueue, shortenText } from '../shared/utility.model';
 import { ModalService } from './molecules/modal/modal.service';
 import { DialogService } from './organisms/custom-dialog';
@@ -20,24 +20,6 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
     styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-    data: AppData;
-    activeTaskList: TaskList | undefined;
-    taskNameInput: string;
-
-    showCompletedMaster = false;
-    showCompleted = this.showCompletedMaster;
-
-    countOpenTasks = countOpenTasks;
-
-    isTouchDevice = isTouchDevice();
-    isMobileMenuOpen: boolean;
-    setMobileMenuOpen = (e: boolean) => (this.isMobileMenuOpen = e);
-
-    quickAddInputField = {
-        focusEventsSubject: [new Subject<boolean>(), new Subject<boolean>()],
-        focus: (inputField: number) => this.quickAddInputField.focusEventsSubject[inputField].next(true),
-    };
-
     constructor(
         public modalService: ModalService,
         private dialogService: DialogService,
@@ -57,25 +39,22 @@ export class AppComponent {
         });
     }
 
-    ///////////////////////////////////////////// Tasks ////////////////////////////////////////////////
+    data: AppData;
+    activeTaskList: TaskList | undefined;
+    taskNameInput: string;
 
-    addTask = [
-        (newTaskName: string) => {
-            if (!newTaskName) return;
-            if (this.data.lists.length == 0) {
-                this.dialogService.confirm({ title: "You don't have any lists." });
-                return;
-            }
-            this.dispatchCreateTask(newTaskName);
-            this.quickAddInputField.focus(0);
-        },
-        (newTaskName: string) => {
-            this.dispatchCreateTask(newTaskName);
-            this.quickAddInputField.focus(1);
-        },
-    ];
-    dispatchCreateTask = (newTaskName: string) =>
-        this.store.dispatch(new AppDataActions.CreateTask(this.activeTaskList.id, newTaskName));
+    showCompleted = false;
+
+    isTouchDevice = isTouchDevice();
+    isMobileMenuOpen: boolean;
+    setMobileMenuOpen = (open: boolean) => (this.isMobileMenuOpen = open);
+
+    quickAddInputField = {
+        focusEventsSubject: [new Subject<boolean>(), new Subject<boolean>()],
+        focus: (inputField: number) => this.quickAddInputField.focusEventsSubject[inputField].next(true),
+    };
+
+    ///////////////////////////////////////////// Tasks ////////////////////////////////////////////////
 
     getTaskById(taskId: string, taskList: Task[], getParentArr = false) {
         const recurse = (taskId: string, arr: Task[]): Task | Task[] | void => {
@@ -91,24 +70,35 @@ export class AppComponent {
         };
         return recurse(taskId, taskList);
     }
-    sortableTaskData: Task[];
-    dropTask(event: CdkDragDrop<string[]>) {
-        moveItemInArray(this.sortableTaskData, event.previousIndex, event.currentIndex);
-        this.sortTasks(this.sortableTaskData);
-    }
-    sortTasks(sortedTasks: Task[]) {
-        console.log('sort Tasks');
-        // this.store.dispatch(new AppDataActions.)
-    }
+
+    dispatchCreateTask = (newTaskName: string) =>
+        this.store.dispatch(new AppDataActions.CreateTask(this.activeTaskList.id, newTaskName));
+    addTask = [
+        (newTaskName: string) => {
+            if (!newTaskName) return;
+            if (this.data.lists.length == 0) {
+                this.dialogService.confirm({ title: "You don't have any lists." });
+                return;
+            }
+            this.dispatchCreateTask(newTaskName);
+            this.quickAddInputField.focus(0);
+        },
+        (newTaskName: string) => {
+            this.dispatchCreateTask(newTaskName);
+            this.quickAddInputField.focus(1);
+        },
+    ];
 
     ///////////////////////////////////////////// Lists ////////////////////////////////////////////////
+
     getListById = (id: string): TaskList | undefined => this.data.lists.find(list => list.id == id);
-    getListIndexById = (id: string): number => this.data.lists.indexOf(this.data.lists.find(list => list.id == id));
+    getListIndexById = (id: string): number => this.data.lists.findIndex(list => list.id == id);
 
     setActiveList = (listId: string) => {
         this.store.dispatch(new AppDataActions.SetActiveList(listId));
         this.isMobileMenuOpen = false;
     };
+
     createList = () =>
         this.dialogService
             .prompt({ title: 'Create new list:', buttons: ['Cancel', 'Create'], placeholder: 'list name' })
@@ -134,7 +124,9 @@ export class AppComponent {
                         .catch(err => {});
             });
     };
+
     deleteList = (listId: string) => this.store.dispatch(new AppDataActions.DeleteList(listId));
+
     sortLists(sortedLists: TaskList[]) {
         this.store.dispatch(new AppDataActions.SortLists(sortedLists));
     }
@@ -154,9 +146,7 @@ export class AppComponent {
                 if ('appData' in jsonData) {
                     const appData = jsonData.appData;
                     if ('activeListId' in appData && 'lists' in appData) {
-                        // const data: AppData = getCopyOf(this.data);
-                        // // TODO: add a prompt wich lets the user select wich lists to import
-                        // data.lists = [...this.data.lists, ...appData.lists];
+                        // TODO: add a prompt wich lets the user select wich lists to import
                         this.appDataService.importFromJSON(appData);
                     } else this.dialogService.confirm({ title: 'The JSON File does not contain the necessary data.' });
                 } else this.dialogService.confirm({ title: 'The JSON File might not be what you think it is.' });
@@ -168,9 +158,8 @@ export class AppComponent {
     };
 
     ngAfterViewInit() {
-        // this.nameInputRef.nativeElement.select();
-        this.quickAddInputField.focus(0);
-
         if (this.isTouchDevice) document.body.classList.add('touchDevice');
+
+        this.quickAddInputField.focus(0);
     }
 }
