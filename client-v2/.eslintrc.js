0bcf00066909f15781c6e4bb9a4ad8b39f8c8e7a
Floyd Haremsa <floyd.haremsa@berlin-bytes.de>
2022-09-11 01:20:24 +0200

    Setup eslint & prettier, format all files

---
+++ client-v2/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        rootDir: './',
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: 'tsconfig.json',
+    },
+    plugins: ['@typescript-eslint'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'prettier'],
+    root: true,
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    overrides: [],
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+        'linebreak-style': ['error', 'unix'],
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    },
+}
