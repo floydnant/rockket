babcc0a7376ea75e5247a4d98af09ea665f8b37d
Floyd Haremsa <floyd.haremsa@berlin-bytes.de>
2022-09-11 19:56:03 +0200

    Add tailwind color config, add script to convert colors from figma export to tailwind

--- client-v2/tailwind.config.js
+++ client-v2/tailwind.config.js
@@ -1,8 +1,10 @@
+const colors = require('./colors.json')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{html,css,ts}'],
     theme: {
-        extend: {},
+        extend: { colors },
     },
     plugins: [],
 }
