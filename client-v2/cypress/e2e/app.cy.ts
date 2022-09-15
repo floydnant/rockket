f5f0f46b32eeb2a0aec9ca81daf220ae0a386014
Floyd Haremsa <floyd.haremsa@berlin-bytes.de>
2022-09-15 21:46:35 +0200

    Add unit test ci run

--- client-v2/cypress/e2e/app.cy.ts
+++ client-v2/cypress/e2e/app.cy.ts
@@ -3,8 +3,4 @@ describe('App', () => {
         cy.visit('/')
         cy.contains('app is running!')
     })
-    it('displays the current app version', () => {
-        cy.visit('/')
-        cy.contains('2.0.0')
-    })
 })
