993c038c59dd2c1069127560f8104bcb75583b0e
Floyd Haremsa <floyd.haremsa@berlin-bytes.de>
2022-09-17 23:03:31 +0200

    Add app layout, prepare component playground

--- client-v2/src/app/app.module.ts
+++ client-v2/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { DemoComponent } from './components/demo/demo.component'
+import { ComponentPlaygroundComponent } from './pages/component-playground/component-playground.component'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
-    declarations: [AppComponent, DemoComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [
+        AppComponent,
+        DemoComponent,
+        ComponentPlaygroundComponent,
+    ],
+    imports: [BrowserModule, FormsModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
 })
---
+++ client-v2/src/app/pages/component-playground/component-playground.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'component-playground',
+    templateUrl: './component-playground.component.html',
+    styleUrls: ['./component-playground.component.css'],
+})
+export class ComponentPlaygroundComponent {
+}
