c66caeef8a2276b963e28570ebd5059f710380b2
Floyd Haremsa <floyd.haremsa@berlin-bytes.de>
2022-09-18 20:28:04 +0200

    Add env config validation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,9 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
-async function bootstrap() {
+async function bootstrap(configService: ConfigService) {
     const app = await NestFactory.create(AppModule)
-    await app.listen(3000)
+    await app.listen(configService.get('PORT'))
 }
-bootstrap()
+bootstrap(new ConfigService())
