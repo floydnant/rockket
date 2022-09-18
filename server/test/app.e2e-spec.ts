c1af56161496ae6ae8600969964158f08578ae40
Floyd Haremsa <floyd.haremsa@berlin-bytes.de>
2022-09-18 03:10:50 +0200

    Update deps, update scripts, add 404 e2e test

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -18,4 +18,7 @@ describe('AppController (e2e)', () => {
     it('/ (GET)', () => {
         return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!')
     })
+    it('/not-found (GET)', () => {
+        return request(app.getHttpServer()).get('/not-found').expect(404)
+    })
 })
