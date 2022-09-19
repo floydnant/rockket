0958840e2cdd189ceb24de1df087f968937af635
Floyd Haremsa <floyd.haremsa@berlin-bytes.de>
2022-09-19 23:17:35 +0200

    Fix syntax error in sh script

--- server/run-e2e.sh
+++ server/run-e2e.sh
@@ -1,6 +1,7 @@
 if [[ -z $TESTING_DATABASE_URL ]];
     then DATABASE_URL=$(cat .env | grep 'TESTING' | sed 's/.*="\(.*\)"/\1/');
-       if [[ -z $DATABASE_URL ]];  echo 'TESTING_DATABASE_URL not specified' && exit 0
+       if [[ -z $DATABASE_URL ]]; then echo 'TESTING_DATABASE_URL not specified' && exit 0
+       fi
     else DATABASE_URL=$TESTING_DATABASE_URL;
 fi;
 
