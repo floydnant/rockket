b9e97cb9941fdd23f20a3592dd9fd678dc9e1eb2
Floyd Haremsa <floyd.haremsa@berlin-bytes.de>
2022-09-19 23:12:38 +0200

    Merge e2e related sh scripts

--- server/get-testing-db-url.sh
+++
@@ -1,6 +0,0 @@
-if [[ -z $1 ]]; 
-    then DATABASE_URL=$(cat .env | grep 'TESTING' | sed 's/.*="\(.*\)"/\1/'); 
-    else DATABASE_URL=$1;
-fi;
-
-echo $DATABASE_URL
--- server/run-e2e.sh
+++ server/run-e2e.sh
@@ -1,3 +1,8 @@
-DATABASE_URL="$(sh ./get-testing-db-url.sh $TESTING_DATABASE_URL)"; 
-npm run db:reset 
+if [[ -z $TESTING_DATABASE_URL ]];
+    then DATABASE_URL=$(cat .env | grep 'TESTING' | sed 's/.*="\(.*\)"/\1/');
+       if [[ -z $DATABASE_URL ]];  echo 'TESTING_DATABASE_URL not specified' && exit 0
+    else DATABASE_URL=$TESTING_DATABASE_URL;
+fi;
+
+npm run db:reset
 jest $1
