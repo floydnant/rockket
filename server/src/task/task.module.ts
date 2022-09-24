623b3bfc73e19624a16ae426f2c4ca3bfd0ebaa8
Floyd Haremsa <floyd.h@gmx.de>
2022-09-24 23:28:34 +0200

    Run linter

--- server/src/task/task.module.ts
+++ server/src/task/task.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { UserModule } from '../user/user.module'
 import { ListController } from './list/list.controller'
-import { TaskController } from './task.controller';
+import { TaskController } from './task.controller'
 
 @Module({
     imports: [UserModule],
