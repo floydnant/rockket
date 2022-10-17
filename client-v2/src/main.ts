4498a0470bf6d405461970f55cd671194c7865e7
Floyd Haremsa <floyd.h@gmx.de>
2022-10-17 17:24:12 +0200

    Add env vars

--- client-v2/src/main.ts
+++ client-v2/src/main.ts
@@ -6,6 +6,8 @@ import { environment } from './environments/environment'
 
 if (environment.production) enableProdMode()
 
+console.info('ServerBaseUrl:', environment.SERVER_BASE_URL)
+
 platformBrowserDynamic()
     .bootstrapModule(AppModule)
     .catch(err => console.error(err))
