26ed872824eb82dcdd4c7d784534591c9f6747ca
Floyd Haremsa <floyd.h@gmx.de>
2022-11-17 22:24:43 +0100

    Add, for now unused, utility function -> isActiveRouteUsingGuard

---
+++ client-v2/src/app/guards/guard-utils.ts
@@ -0,0 +1,17 @@
+import { Router } from '@angular/router'
+
+/** Check if the currently active route is using the specified guard.
+ *
+ * **Note:** Only works with root level routes, not with child routes */
+export const isActiveRouteUsingGuard = (
+    router: Router,
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    /** The RouteGuard to check for */ guard: Function
+) => {
+    const currentRoutePath = router.url.match(/^\/\w+/)?.[0].replace(/\//, '')
+    const currentRouteConfig = router.config.find(route => route.path == currentRoutePath)
+
+    const usesAuthGuard = currentRouteConfig?.canActivate?.some(routeGuard => routeGuard instanceof guard) || false
+
+    return usesAuthGuard
+}
