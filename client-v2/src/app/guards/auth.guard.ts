8703a3338452eafaa44460aff466bc52e3231285
Floyd Haremsa <floyd.h@gmx.de>
2022-11-25 19:27:19 +0100

    Fix e2e tests

--- client-v2/src/app/guards/auth.guard.ts
+++ client-v2/src/app/guards/auth.guard.ts
@@ -3,7 +3,6 @@ import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from
 import { Store } from '@ngrx/store'
 import { map, take } from 'rxjs'
 import { AppState } from '../store'
-import { userSelectors } from '../store/user/user.selectors'
 
 @Injectable({
     providedIn: 'root',
@@ -12,15 +11,17 @@ export class AuthGuard implements CanActivate {
     constructor(private store: Store<AppState>, private router: Router) {}
 
     canActivate(_route: ActivatedRouteSnapshot, routerState: RouterStateSnapshot) {
-        return this.store.select(userSelectors.selectLoginState).pipe(
-            // filter(userState => !userState.isLoading), // wait for ongoing login process to complete
-            map(({ isLoggedIn, isLoading }) => {
-                if (isLoggedIn) return true
+        return this.store
+            .select(state => state.user)
+            .pipe(
+                // filter(userState => !userState.isLoading), // wait for ongoing login process to complete
+                map(({ isLoggedIn, isLoading }) => {
+                    if (isLoggedIn) return true
 
-                if (isLoading) return this.router.parseUrl(`/auth/login-loading?callback=${routerState.url}`)
-                return this.router.parseUrl(`/auth?callback=${routerState.url}`)
-            }),
-            take(1) // not sure if this is necessary, but better safe than sorry
-        )
+                    if (isLoading) return this.router.parseUrl(`/auth/login-loading?callback=${routerState.url}`)
+                    return this.router.parseUrl(`/auth?callback=${routerState.url}`)
+                }),
+                take(1) // not sure if this is necessary, but better safe than sorry
+            )
     }
 }
