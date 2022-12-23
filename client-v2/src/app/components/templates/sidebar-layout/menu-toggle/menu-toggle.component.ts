1e57792d2179e764703ba62e658d6d5de361dad0
Floyd Haremsa <floyd.h@gmx.de>
2022-12-23 23:31:03 +0100

    Fix component tests

--- client-v2/src/app/components/templates/sidebar-layout/menu-toggle/menu-toggle.component.ts
+++ client-v2/src/app/components/templates/sidebar-layout/menu-toggle/menu-toggle.component.ts
@@ -1,10 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
 import { MenuService } from '../menu.service'
 
 @Component({
     selector: 'app-menu-toggle',
     template: `
-        <button class="py-2 px-4 md:hidden" (click)="toggleIsOpen()" data-test-name="menu-toggle-2">
+        <button
+            class="py-2 px-4 md:hidden"
+            (click)="toggleIsOpen()"
+            data-test-name="menu-toggle"
+            [attr.data-test-name]="'menu-toggle-' + index"
+        >
             <app-icon *ngIf="isMenuOpen$ | async; else elseBlock" iconClass="fas fa-times"></app-icon>
             <ng-template #elseBlock>
                 <app-icon iconClass="far fa-bars"></app-icon>
@@ -16,6 +21,8 @@ import { MenuService } from '../menu.service'
 })
 export class MenuToggleComponent {
     constructor(private menuService: MenuService) {}
+
+    @Input() index: string | number = 0
 
     isMenuOpen$ = this.menuService.isMenuOpen$
     toggleIsOpen() {
