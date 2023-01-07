e600feb255eef5dae743add8a7afc40438a95ed7
Floyd Haremsa <floyd.h@gmx.de>
2023-01-07 02:23:06 +0100

    Fix unit tests

--- client-v2/src/app/directives/tooltip.directive.spec.ts
+++ client-v2/src/app/directives/tooltip.directive.spec.ts
@@ -1,18 +1,18 @@
-describe('TooltipDirective', () => {
-    // let directive: TooltipDirective
-    // beforeEach(async () => {
-    //     await TestBed.configureTestingModule({
-    //         declarations: [TooltipComponent],
-    //         providers: [
-    //             TooltipDirective,
-    //             HTMLElement,
-    //             ElementRef,
-    //             { provide: TOOLTIP_DATA, useValue: 'testing tooltip' },
-    //         ],
-    //     })
-    //     directive = TestBed.inject(TooltipDirective)
-    // })
-    // it('should create an instance', () => {
-    //     expect(directive).toBeTruthy()
-    // })
-})
+// describe('TooltipDirective', () => {
+//     // let directive: TooltipDirective
+//     // beforeEach(async () => {
+//     //     await TestBed.configureTestingModule({
+//     //         declarations: [TooltipComponent],
+//     //         providers: [
+//     //             TooltipDirective,
+//     //             HTMLElement,
+//     //             ElementRef,
+//     //             { provide: TOOLTIP_DATA, useValue: 'testing tooltip' },
+//     //         ],
+//     //     })
+//     //     directive = TestBed.inject(TooltipDirective)
+//     // })
+//     // it('should create an instance', () => {
+//     //     expect(directive).toBeTruthy()
+//     // })
+// })
