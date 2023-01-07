e5bf189d199867c194374b6e98fa4ee2b2a9926c
Floyd Haremsa <floyd.h@gmx.de>
2023-01-07 16:50:02 +0100

    Add a script that checks if the current pr has the label "dependencies"

---
+++ check-dependencies-label-for-pr.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Set the owner and repository name
+owner="dein-ding"
+repo="todo-app"
+
+# Get the pull request number from the command line argument
+pull_number=$1
+
+# Send the GET request to the GitHub API
+response=$(curl -s -X GET https://api.github.com/repos/$owner/$repo/pulls/$pull_number -H "Accept: application/vnd.github+json")
+
+# Use the jq filter to check if the 'dependencies' label exists
+output=$(echo "$response" | jq '.labels | map(select(.name == "dependencies")) | length')
+
+# Check the output and exit with the appropriate exit code
+if [ "$output" -eq 0 ]; then
+    exit 1
+else
+    exit 0
+fi
