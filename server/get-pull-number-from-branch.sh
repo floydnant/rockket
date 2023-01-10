2107a178fdebabd9bb4644f3fd7543fb79ad2c40
Floyd Haremsa <floyd.h@gmx.de>
2023-01-10 21:11:47 +0100

    Switch from using jq to javascript

--- server/get-pull-number-from-branch.sh
+++ server/get-pull-number-from-branch.sh
@@ -5,8 +5,34 @@ repo='dein-ding/todo-app'
 # The branch for which you want to get the pull request number
 branch=$1
 
-# Get the pull request number
-pull_request_number=$(curl -s https://api.github.com/repos/$repo/pulls\?state\=all | jq ".[] | select(.head.ref == \"$branch\") | .number")
+# retrieve the pull request information for the specified branch
+response=$(curl -s https://api.github.com/repos/$repo/pulls\?state\=all)
 
-# Print the pull request number
-echo $pull_request_number
+# check if the API returned a valid response
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to retrieve information from the API."
+    exit 1
+fi
+
+# We chould have just went with `jq`, but railway decided not to provide that tool
+# pull_request_number=$($response | jq ".[] | select(.head.ref == \"$branch\") | .number")
+# echo $pull_request_number
+
+# So we'll need to go with js
+pull_request_number=$(node -e "
+const resRaw = process.argv[1]
+const res = JSON.parse(resRaw)
+
+const pullNumber = res.find(pr => pr.head.ref == '$branch').number
+console.log(pullNumber)
+" "$response")
+
+# check if the pull request number was found
+if [ -z "$pull_request_number" ]; then
+    echo "Error: Pull request for the branch not found."
+    exit 1
+else
+    # print the pull request number
+    echo "Pull request number: $pull_request_number"
+    exit 0
+fi
