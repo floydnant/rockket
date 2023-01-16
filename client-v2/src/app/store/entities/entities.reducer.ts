8eb353b7d0f26ce85b968702d72b6d50958325e3
Floyd Haremsa <floyd.h@gmx.de>
2023-01-16 23:47:49 +0100

    Fix unit tests

--- client-v2/src/app/store/entities/entities.reducer.ts
+++ client-v2/src/app/store/entities/entities.reducer.ts
@@ -4,7 +4,7 @@ import { TasklistDetail } from 'src/app/fullstack-shared-models/list.model'
 import { TaskPreview } from 'src/app/fullstack-shared-models/task.model'
 import { entitiesActions, listActions, taskActions } from './entities.actions'
 import { EntitiesState } from './entities.state'
-import { getParentByChildId, getEntityById, buildEntityTree } from './utils'
+import { getParentEntityByChildId, getEntityById, buildEntityTree } from './utils'
 
 const initialState: EntitiesState = {
     entityTree: null,
@@ -56,7 +56,7 @@ export const entitiesReducer = createReducer(
 
     on(entitiesActions.deleteSuccess, (state, { id }): EntitiesState => {
         const entityTreeCopy = structuredClone(state.entityTree)
-        const result = getParentByChildId(entityTreeCopy, id)
+        const result = getParentEntityByChildId(entityTreeCopy, id)
         if (!result) return state
 
         result.subTree.splice(result.index, 1)
--- client-v2/src/app/store/entities/utils.spec.ts
+++ client-v2/src/app/store/entities/utils.spec.ts
@@ -1,55 +1,68 @@
-import { EntityPreview, EntityPreviewFlattend, EntityPreviewRecursive } from 'src/app/models/entities.model'
-import { buildEntityTree, flattenEntityTree, getParentByChildId, traceEntity } from './utils'
+import {
+    EntityPreview,
+    EntityPreviewFlattend,
+    EntityPreviewRecursive,
+    EntityType,
+} from 'src/app/fullstack-shared-models/entities.model'
+import { buildEntityTree, flattenEntityTree, getParentEntityByChildId, traceEntity } from './utils'
 
 const entityPreviewsFixture: EntityPreview[] = [
-    { id: '1', title: 'First', childLists: ['3'], parentListId: '' },
-    { id: '2', title: 'Second', childLists: [], parentListId: '' },
-    { id: '3', title: 'First nested', childLists: ['4'], parentListId: '1' },
-    { id: '4', title: 'Deep nested', childLists: [], parentListId: '3' },
+    { id: '1', entityType: EntityType.TASKLIST, title: 'First', parentId: '' },
+    { id: '2', entityType: EntityType.TASKLIST, title: 'Second', parentId: '' },
+    { id: '3', entityType: EntityType.TASKLIST, title: 'First nested', parentId: '1' },
+    { id: '4', entityType: EntityType.TASKLIST, title: 'Deep nested', parentId: '3' },
 ]
 const entityTreeFixture: EntityPreviewRecursive[] = [
     {
         id: '1',
+        entityType: EntityType.TASKLIST,
         title: 'First',
-        parentListId: '',
+        parentId: '',
         children: [
             {
                 id: '3',
+                entityType: EntityType.TASKLIST,
                 title: 'First nested',
-                parentListId: '1',
-                children: [{ id: '4', title: 'Deep nested', children: [], parentListId: '3' }],
+                parentId: '1',
+                children: [
+                    { id: '4', title: 'Deep nested', children: [], parentId: '3', entityType: EntityType.TASKLIST },
+                ],
             },
         ],
     },
-    { id: '2', title: 'Second', parentListId: '', children: [] },
+    { id: '2', entityType: EntityType.TASKLIST, title: 'Second', parentId: '', children: [] },
 ]
 
 const entityTreeFlattenedFixture: EntityPreviewFlattend[] = [
     {
         id: '1',
+        entityType: EntityType.TASKLIST,
         title: 'First',
-        parentListId: '',
+        parentId: '',
         childrenCount: 1,
         path: [],
     },
     {
         id: '3',
+        entityType: EntityType.TASKLIST,
         title: 'First nested',
-        parentListId: '1',
+        parentId: '1',
         childrenCount: 1,
         path: ['1'],
     },
     {
         id: '4',
+        entityType: EntityType.TASKLIST,
         title: 'Deep nested',
-        parentListId: '3',
+        parentId: '3',
         childrenCount: 0,
         path: ['1', '3'],
     },
     {
         id: '2',
+        entityType: EntityType.TASKLIST,
         title: 'Second',
-        parentListId: '',
+        parentId: '',
         childrenCount: 0,
         path: [],
     },
@@ -57,54 +70,69 @@ const entityTreeFlattenedFixture: EntityPreviewFlattend[] = [
 
 // @TODO: Write proper tests with jest
 describe('Store entity utils', () => {
-    describe('buildEntityTree()', () => {
+    describe(`${buildEntityTree.name}()`, () => {
         it('should return the correct tree', () => {
             const entityTree = buildEntityTree(entityPreviewsFixture)
             expect(entityTree).toEqual(entityTreeFixture)
         })
     })
 
-    describe('flattenEntityTree()', () => {
+    describe(`${flattenEntityTree.name}()`, () => {
         it('should return the correct flattened equivalent of the tree', () => {
             const entityTreeFlattened = flattenEntityTree(entityTreeFixture)
             expect(entityTreeFlattened).toEqual(entityTreeFlattenedFixture)
         })
     })
 
-    describe('traceEntity()', () => {
+    describe(`${traceEntity.name}()`, () => {
         it('should return the correct path to the given entity in the tree', () => {
             const trace = traceEntity(entityTreeFixture, '4')
             const traceFixture: EntityPreviewRecursive[] = [
                 {
                     id: '1',
+                    entityType: EntityType.TASKLIST,
                     title: 'First',
-                    parentListId: '',
+                    parentId: '',
                     children: [
                         {
                             id: '3',
+                            entityType: EntityType.TASKLIST,
                             title: 'First nested',
-                            parentListId: '1',
-                            children: [{ id: '4', title: 'Deep nested', children: [], parentListId: '3' }],
+                            parentId: '1',
+                            children: [
+                                {
+                                    id: '4',
+                                    entityType: EntityType.TASKLIST,
+                                    title: 'Deep nested',
+                                    children: [],
+                                    parentId: '3',
+                                },
+                            ],
                         },
                     ],
                 },
                 {
                     id: '3',
+                    entityType: EntityType.TASKLIST,
                     title: 'First nested',
-                    parentListId: '1',
-                    children: [{ id: '4', title: 'Deep nested', children: [], parentListId: '3' }],
+                    parentId: '1',
+                    children: [
+                        { id: '4', entityType: EntityType.TASKLIST, title: 'Deep nested', children: [], parentId: '3' },
+                    ],
                 },
-                { id: '4', title: 'Deep nested', children: [], parentListId: '3' },
+                { id: '4', entityType: EntityType.TASKLIST, title: 'Deep nested', children: [], parentId: '3' },
             ]
             expect(trace).toEqual(traceFixture)
         })
     })
 
-    describe('getParentByChildId()', () => {
+    describe(`${getParentEntityByChildId.name}()`, () => {
         it('should return the correct result', () => {
-            const result = getParentByChildId(entityTreeFixture, '4')
-            const resultFixture: ReturnType<typeof getParentByChildId> = {
-                subTree: [{ id: '4', title: 'Deep nested', children: [], parentListId: '3' }],
+            const result = getParentEntityByChildId(entityTreeFixture, '4')
+            const resultFixture: ReturnType<typeof getParentEntityByChildId> = {
+                subTree: [
+                    { id: '4', entityType: EntityType.TASKLIST, title: 'Deep nested', children: [], parentId: '3' },
+                ],
                 index: 0,
             }
 
