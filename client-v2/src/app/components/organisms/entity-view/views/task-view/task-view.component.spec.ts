cbc02c6bed02a3550bd941e85d4a4d31649a4fd6
Floyd Haremsa <floyd.h@gmx.de>
2023-01-23 01:32:08 +0100

    Fix unit testss

--- client-v2/src/app/components/organisms/entity-view/views/task-view/task-view.component.spec.ts
+++ client-v2/src/app/components/organisms/entity-view/views/task-view/task-view.component.spec.ts
@@ -1,4 +1,8 @@
+import { AsyncPipe } from '@angular/common'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { BehaviorSubject } from 'rxjs'
+import { storeMock } from 'src/app/utils/unit-test.mocks'
+import { EntityViewComponent, EntityViewData, ENTITY_VIEW_DATA } from '../../entity-view.component'
 
 import { TaskViewComponent } from './task-view.component'
 
@@ -7,8 +11,20 @@ describe('TaskViewComponent', () => {
     let fixture: ComponentFixture<TaskViewComponent>
 
     beforeEach(async () => {
+        const entityViewDataMock: EntityViewData<any> = {
+            detail$: new BehaviorSubject(null),
+            entity$: new BehaviorSubject(null),
+            options$: new BehaviorSubject(null),
+        }
         await TestBed.configureTestingModule({
             declarations: [TaskViewComponent],
+            providers: [
+                { provide: ENTITY_VIEW_DATA, useValue: entityViewDataMock },
+                storeMock,
+                // eslint-disable-next-line @typescript-eslint/no-empty-function
+                { provide: EntityViewComponent, useValue: { progress$: { next() {} } } },
+            ],
+            imports: [AsyncPipe],
         }).compileComponents()
 
         fixture = TestBed.createComponent(TaskViewComponent)
