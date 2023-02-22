4f7d79c559d113e5e9d2bf8ab0c86340efb19e74
Floyd Haremsa <floyd.h@gmx.de>
2023-02-22 09:03:38 +0100

    Refactor sidebar resize logic with rxjs

--- client-v2/src/app/components/templates/sidebar-layout/sidebar-layout.component.ts
+++ client-v2/src/app/components/templates/sidebar-layout/sidebar-layout.component.ts
@@ -1,13 +1,16 @@
 import { AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core'
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy'
+import { fromEvent, switchMap, takeUntil, tap } from 'rxjs'
 import { DeviceService } from 'src/app/services/device.service'
 import { MenuService } from './menu.service'
 
+@UntilDestroy()
 @Component({
     selector: 'app-sidebar-layout',
     templateUrl: './sidebar-layout.component.html',
     styleUrls: ['./sidebar-layout.component.css'],
 })
-export class SidebarLayoutComponent implements AfterViewInit, OnDestroy {
+export class SidebarLayoutComponent implements AfterViewInit {
     constructor(private menuService: MenuService, private deviceService: DeviceService) {}
 
     @Input() enableResize = true
@@ -25,27 +28,22 @@ export class SidebarLayoutComponent implements AfterViewInit, OnDestroy {
     @ViewChild('resizeHandle') resizeHandle!: ElementRef<HTMLDivElement>
 
     ngAfterViewInit(): void {
-        if (this.enableResize) {
-            this.resizeHandle.nativeElement.addEventListener('mousedown', this.onMouseDown)
-            document.addEventListener('mouseup', this.onMouseUp)
-        }
-    }
-    ngOnDestroy(): void {
-        if (this.enableResize) {
-            this.resizeHandle.nativeElement.removeEventListener('mousedown', this.onMouseDown)
-            document.removeEventListener('mouseup', this.onMouseUp)
-        }
+        if (!this.enableResize) return
+
+        this.setupResize().subscribe()
     }
 
-    // in case: https://stackoverflow.com/questions/26233180/resize-a-div-on-border-drag-and-drop-without-adding-extra-markup
-
-    onMouseDown = (() => document.addEventListener('mousemove', this.onResize)).bind(this)
-    onMouseUp = (() => document.removeEventListener('mousemove', this.onResize)).bind(this)
-
-    onResize = ((e: MouseEvent) => {
-        const sidebar = this.resizeHandle.nativeElement.parentElement as HTMLDivElement
-        const currMouseX = e.clientX
-        const newWidth = currMouseX + 'px'
-        sidebar.style.width = newWidth
-    }).bind(this)
+    setupResize = () =>
+        fromEvent(this.resizeHandle.nativeElement, 'mousedown').pipe(
+            switchMap(() =>
+                fromEvent<MouseEvent>(document, 'mousemove').pipe(takeUntil(fromEvent(document, 'mouseup')))
+            ),
+            tap(e => {
+                const sidebar = this.resizeHandle.nativeElement.parentElement as HTMLDivElement
+                const currMouseX = e.clientX
+                const newWidth = currMouseX + 'px'
+                sidebar.style.width = newWidth
+            }),
+            untilDestroyed(this)
+        )
 }
