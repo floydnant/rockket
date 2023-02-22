9642ad34cccc40e809ad5ee3a3ef124b6a0b690d
Floyd Haremsa <floyd.h@gmx.de>
2023-02-22 09:41:08 +0100

    Clean up mutation.directive

--- client-v2/src/app/directives/mutation.directive.ts
+++ client-v2/src/app/directives/mutation.directive.ts
@@ -1,58 +1,61 @@
-import { Directive, ElementRef, EventEmitter, Input, OnChanges, OnDestroy, Output, SimpleChanges } from '@angular/core'
+import { Directive, ElementRef, EventEmitter, Input, OnDestroy, Output } from '@angular/core'
+
+interface MutationOptions {
+    plainOnly?: boolean
+    observe?: boolean
+}
 
 @Directive({
     selector: '[domChanges], [textChanges]',
 })
-export class MutationDirective implements OnDestroy, OnChanges {
-    constructor(private elementRef: ElementRef<HTMLElement>) {
-        const element = this.elementRef.nativeElement
+export class MutationDirective implements OnDestroy {
+    constructor(private elementRef: ElementRef<HTMLElement>) {}
 
-        this.observer = new MutationObserver(mutations => {
-            mutations.forEach(mutation => {
-                if (this.mutationOptions.plainOnly && element.childNodes.length > 1) {
-                    element.innerHTML = element.innerText.trim()
-
-                    try {
-                        window.getSelection()?.setPosition(element, 1) // restore cursor position
-                    } catch (err) {
-                        console.log('Error restoring cursor position', err)
-                    }
-                    return
-                }
-
-                this.domChanges.emit(mutation)
-                if (this.mutationOptions.plainOnly) this.textChanges.emit(element.innerText.trim())
-                else this.textChanges.emit(element.innerText.trim() ? element.innerHTML.trim() : '')
-            })
-        })
-    }
-
-    ngOnChanges(changes: SimpleChanges): void {
-        if ('mutationOptions' in changes)
-            if (!this.mutationOptions.observe) this.observer.disconnect()
-            else {
-                this.observer.observe(this.elementRef.nativeElement, {
-                    attributes: false,
-                    childList: true,
-                    subtree: true,
-                    characterData: true,
-                })
-            }
-    }
     ngOnDestroy(): void {
         this.observer.disconnect()
     }
 
-    private observer: MutationObserver
-
     @Output() domChanges = new EventEmitter<MutationRecord>()
     @Output() textChanges = new EventEmitter<string>()
 
-    @Input() mutationOptions: {
-        plainOnly?: boolean
-        observe?: boolean
-    } = {
+    @Input('mutationOptions') set setMutationOptions(options: MutationOptions) {
+        this.mutationOptions = options
+
+        if (!options.observe) {
+            this.observer.disconnect()
+            return
+        }
+
+        this.observer.observe(this.elementRef.nativeElement, {
+            attributes: false,
+            childList: true,
+            subtree: true,
+            characterData: true,
+        })
+    }
+    private mutationOptions: MutationOptions = {
         plainOnly: false,
         observe: true,
     }
+
+    private observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            const element = this.elementRef.nativeElement
+
+            if (this.mutationOptions.plainOnly && element.childNodes.length > 1) {
+                element.innerHTML = element.innerText.trim()
+
+                try {
+                    window.getSelection()?.setPosition(element, 1) // restore cursor position
+                } catch (err) {
+                    console.log('Error restoring cursor position', err)
+                }
+                return
+            }
+
+            this.domChanges.emit(mutation)
+            if (this.mutationOptions.plainOnly) this.textChanges.emit(element.innerText.trim())
+            else this.textChanges.emit(element.innerText.trim() ? element.innerHTML.trim() : '')
+        })
+    })
 }
