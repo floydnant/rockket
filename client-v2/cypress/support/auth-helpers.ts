ffa2db2eaad7738aedef681d05c1e38b9a8ba057
Floyd Haremsa <floyd.h@gmx.de>
2023-02-26 14:15:21 +0100

    Reduce typing delay for auth procedures to 5ms

--- client-v2/cypress/support/auth-helpers.ts
+++ client-v2/cypress/support/auth-helpers.ts
@@ -2,10 +2,10 @@ import { credentials, Credentials } from 'cypress/fixtures/user-credentials'
 import { testName } from './helpers'
 
 export const typeSignupCredentialsAndSubmit = (creds: Credentials = credentials['jonathan']) => {
-    cy.get(testName('input-username')).type(creds.username)
-    cy.get(testName('input-email')).type(creds.email)
-    cy.get(testName('input-password')).type(creds.password)
-    cy.get(testName('input-confirmPassword')).type(creds.password)
+    cy.get(testName('input-username')).type(creds.username, { delay: 5 })
+    cy.get(testName('input-email')).type(creds.email, { delay: 5 })
+    cy.get(testName('input-password')).type(creds.password, { delay: 5 })
+    cy.get(testName('input-confirmPassword')).type(creds.password, { delay: 5 })
     cy.get(testName('submit-button')).click()
 }
 export const signupProcedure = (creds: Credentials = credentials['jonathan']) => {
@@ -17,8 +17,8 @@ export const signupProcedure = (creds: Credentials = credentials['jonathan']) =>
 }
 
 export const typeLoginCredentialsAndSubmit = (creds: Credentials = credentials['jonathan']) => {
-    cy.get(testName('input-email')).type(creds.email)
-    cy.get(testName('input-password')).type(creds.password)
+    cy.get(testName('input-email')).type(creds.email, { delay: 5 })
+    cy.get(testName('input-password')).type(creds.password, { delay: 5 })
     cy.get(testName('submit-button')).click()
 }
 export const loginProcedure = (creds: Credentials = credentials['jonathan']) => {
