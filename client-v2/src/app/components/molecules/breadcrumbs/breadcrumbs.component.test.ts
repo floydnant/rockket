5cea5a6c517e720dce2c4fcbbfe0985f85c74fa1
Floyd Haremsa <floyd.h@gmx.de>
2023-04-13 19:26:05 +0200

    Add truncation to breadcrumbs

--- client-v2/src/app/components/molecules/breadcrumbs/breadcrumbs.component.test.ts
+++ client-v2/src/app/components/molecules/breadcrumbs/breadcrumbs.component.test.ts
@@ -4,6 +4,9 @@ import { EntityPageLabelComponent } from '../../atoms/entity-page-label/entity-p
 import { IconsModule } from '../../atoms/icons/icons.module'
 import { DropDownComponent, MenuItem } from '../drop-down/drop-down.component'
 import { Breadcrumb, BreadcrumbsComponent } from './breadcrumbs.component'
+import { OverlayModule } from '@angular/cdk/overlay'
+import { DeviceService } from 'src/app/services/device.service'
+import { TooltipComponent } from '../../atoms/tooltip/tooltip.component'
 
 const defaultTemplate = `<app-breadcrumbs [breadcrumbs]="breadcrumbs"></app-breadcrumbs>`
 const setupComponent = (breadcrumbs: Breadcrumb[], template = defaultTemplate) => {
@@ -11,13 +14,15 @@ const setupComponent = (breadcrumbs: Breadcrumb[], template = defaultTemplate) =
         componentProperties: {
             breadcrumbs,
         },
-        imports: [CdkMenuModule, IconsModule],
-        declarations: [BreadcrumbsComponent, EntityPageLabelComponent, DropDownComponent],
+        imports: [CdkMenuModule, IconsModule, OverlayModule],
+        declarations: [BreadcrumbsComponent, EntityPageLabelComponent, DropDownComponent, TooltipComponent],
+        providers: [DeviceService],
     })
 }
 
 describe('BreadcrumbsComponent', () => {
     it('displays given breadcrumbs', () => {
+        cy.viewport('macbook-13')
         const breadcrumbs: Breadcrumb[] = [
             { title: 'Root list', icon: 'workspace', route: '/' },
             { title: 'Nested list', icon: 'workspace', route: '/nested' },
@@ -32,6 +37,7 @@ describe('BreadcrumbsComponent', () => {
     // @TODO: Navigation through clicking a breadcrumb must be tested in e2e
 
     it('can open a context menu', () => {
+        cy.viewport('macbook-13')
         const menuItems: MenuItem[] = [{ title: 'test' }]
         const breadcrumbs: Breadcrumb[] = [
             { title: 'Root list', icon: 'workspace', route: '/', contextMenuItems: menuItems },
@@ -43,5 +49,53 @@ describe('BreadcrumbsComponent', () => {
         cy.get(testName('drop-down-menu')).should('not.exist')
         cy.get(testName('breadcrumb')).first().rightclick()
         cy.get(testName('drop-down-menu')).should('exist')
+    })
+
+    describe('Truncation', () => {
+        const menuItems: MenuItem[] = [{ title: 'test' }]
+        const breadcrumbs: Breadcrumb[] = [
+            { title: 'Root list', icon: 'workspace', route: '/', contextMenuItems: menuItems },
+            { title: '1. Nested list', icon: 'workspace', route: '/ne', contextMenuItems: menuItems },
+            { title: '2. Nested list', icon: 'workspace', route: '/ne/ne', contextMenuItems: menuItems },
+            { title: '3. Nested list', icon: 'workspace', route: '/ne/ne/ne', contextMenuItems: menuItems },
+            { title: '4. Nested list', icon: 'workspace', route: '/ne/ne/ne/ne', contextMenuItems: menuItems },
+            { title: '5. Nested list', icon: 'workspace', route: '/ne/ne/ne/ne/ne', contextMenuItems: menuItems },
+            { title: '6. Nested list', icon: 'workspace', route: '/ne/ne/ne/ne/ne/ne', contextMenuItems: menuItems },
+        ]
+
+        it('truncates breadcrumbs on desktop', () => {
+            cy.viewport('macbook-13')
+
+            setupComponent(breadcrumbs)
+
+            cy.get(testName('breadcrumb')).should('have.length', 4)
+            cy.get(testName('truncation-breadcrumb')).should('exist')
+        })
+
+        it('truncates breadcrumbs on mobile', () => {
+            setupComponent(breadcrumbs)
+
+            cy.get(testName('breadcrumb')).should('have.length', 1)
+            cy.get(testName('truncation-breadcrumb')).should('exist')
+        })
+
+        it('can show the truncated breadcrumbs', () => {
+            setupComponent(breadcrumbs)
+
+            cy.get(testName('truncation-breadcrumb')).click()
+            cy.get(testName('truncated-breadcrumbs-container')).within(() => {
+                cy.get(testName('breadcrumb')).should('have.length', 6)
+            })
+        })
+
+        it('can access context menu', () => {
+            setupComponent(breadcrumbs)
+
+            cy.get(testName('truncation-breadcrumb')).click()
+            cy.get(testName('truncated-breadcrumbs-container')).within(() => {
+                cy.get(testName('breadcrumb')).first().rightclick()
+            })
+            cy.get(testName('drop-down-menu')).should('exist')
+        })
     })
 })
--- client-v2/src/app/components/molecules/breadcrumbs/breadcrumbs.component.ts
+++ client-v2/src/app/components/molecules/breadcrumbs/breadcrumbs.component.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
 import { IconKey } from '../../atoms/icons/icon/icons'
 import { MenuItem } from '../drop-down/drop-down.component'
+import { BehaviorSubject, combineLatestWith, first, map } from 'rxjs'
+import { moveToMacroQueue } from 'src/app/utils'
+import { DeviceService } from 'src/app/services/device.service'
 
 export interface Breadcrumb {
     title: string
@@ -8,6 +11,14 @@ export interface Breadcrumb {
     route: string
     contextMenuItems?: MenuItem[]
 }
+type BreadcrumbInternal =
+    | Breadcrumb
+    | {
+          truncated: Breadcrumb[]
+      }
+
+const isTruncationBreadcrumb = (breadcrumb: BreadcrumbInternal): breadcrumb is { truncated: Breadcrumb[] } =>
+    'truncated' in breadcrumb
 
 @Component({
     selector: 'app-breadcrumbs',
@@ -22,9 +33,51 @@ export interface Breadcrumb {
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BreadcrumbsComponent {
-    @Input() breadcrumbs!: Breadcrumb[]
+    constructor(private deviceService: DeviceService) {}
 
-    trackByFn(_index: number, { icon, title, route }: Breadcrumb) {
+    @Input() set breadcrumbs(breadcrumbs: Breadcrumb[]) {
+        this.breadcrumbs$_.next(breadcrumbs)
+    }
+    breadcrumbs$_ = new BehaviorSubject<Breadcrumb[]>([])
+    breadcrumbsData$ = this.breadcrumbs$_.pipe(
+        combineLatestWith(this.deviceService.isMobileScreen$),
+        map(([breadcrumbs, isMobileScreen]): BreadcrumbInternal[] => {
+            if (breadcrumbs.length > 4 && !isMobileScreen) {
+                // @TODO: this functionality could be enhanced by adapting the number of breadcrumbs to the actual screen size
+                // -> this setup is awkward for screens between 768px and 1150px
+                const truncationBreadcrumb: BreadcrumbInternal = { truncated: breadcrumbs.slice(1, -3) }
+                const crumbs = [breadcrumbs[0], truncationBreadcrumb, ...breadcrumbs.slice(-3)]
+
+                return crumbs
+            }
+
+            if (breadcrumbs.length > 1 && isMobileScreen) {
+                const truncationBreadcrumb: BreadcrumbInternal = { truncated: breadcrumbs.slice(0, -1) }
+                const crumbs = [truncationBreadcrumb, breadcrumbs[breadcrumbs.length - 1]]
+
+                return crumbs
+            }
+
+            return breadcrumbs
+        })
+    )
+
+    trackByFn(_index: number, crumb: BreadcrumbInternal): string {
+        if (isTruncationBreadcrumb(crumb)) {
+            return 'truncated'
+            // return crumb.truncated.map((c, i) => this.trackByFn(i, c)).join('')
+        }
+
+        const { icon, title, route } = crumb
         return icon + title + route
+    }
+
+    isTruncationBreadcrumb = isTruncationBreadcrumb
+
+    isOverlayOpen$ = new BehaviorSubject(false)
+    closeOverlayDelayed() {
+        moveToMacroQueue(() => {
+            this.isOverlayOpen$.next(false)
+        })
     }
 }
