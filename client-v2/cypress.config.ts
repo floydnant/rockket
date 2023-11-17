c24491d0c6d8b0ead48c42aafc1aa3bdce78b930
Floyd Haremsa <floyd.h@gmx.de>
2023-11-17 10:18:10 +0100

    ci: upload all cy artifacts

--- client-v2/cypress.config.ts
+++ client-v2/cypress.config.ts
@@ -4,7 +4,11 @@ export default defineConfig({
     e2e: {
         baseUrl: 'http://localhost:4200',
         supportFile: './cypress/support/e2e.ts',
+
         video: true,
+        videosFolder: './.cypress/e2e/videos',
+        screenshotsFolder: './.cypress/e2e/screenshots',
+        downloadsFolder: './.cypress/e2e/downloads',
     },
 
     component: {
@@ -13,6 +17,10 @@ export default defineConfig({
             bundler: 'webpack',
         },
         specPattern: '**/*.test.ts',
+
         video: true,
+        videosFolder: './.cypress/component/videos',
+        screenshotsFolder: './.cypress/component/screenshots',
+        downloadsFolder: './.cypress/component/downloads',
     },
 })
