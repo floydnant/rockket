2ab9bbfd021af5b4baafda90060dc79a925612a4
Floyd Haremsa <floyd.h@gmx.de>
2023-11-26 00:03:19 +0100

    test: extend workspace e2e test suite

--- client-v2/cypress/e2e/workspace.cy.ts
+++ client-v2/cypress/e2e/workspace.cy.ts
@@ -74,7 +74,7 @@ describe('Workspace', () => {
                 cy.get(testName('entity-tree-node')).should('contain.text', entityName)
             })
 
-            it('can edit the description', () => {
+            it('can add a description', () => {
                 cy.get(testName('sidebar-create-new-list')).click()
                 cy.get(testName('editable-entity-name'))
 
@@ -91,6 +91,35 @@ describe('Workspace', () => {
                 cy.get(testName('description-editor')).focused().type(description).blur()
                 cy.wait('@updateList').its('response.statusCode').should('equal', 200) // we currently don't have any other way to verify if updating the description has succeeded
                 // maybe it is not a bad idea to assert on the request, but we could take this a step further and verify that the db record was updated
+            })
+
+            it('can update the description', () => {
+                cy.get(testName('sidebar-create-new-list')).click()
+                cy.get(testName('editable-entity-name'))
+
+                // wait for loading to finish
+                cy.get(testName('entity-name-container')).within(() => {
+                    cy.get('[data-test-is-loading="false"]')
+                })
+
+                cy.get(testName('add-description')).click()
+                cy.get(testName('add-description')).should('not.exist')
+
+                const description = 'The testing entity description'
+                cy.get(testName('description-editor')).focused().type(description).blur()
+                cy.wait('@updateList').its('response.statusCode').should('equal', 200)
+
+                // wait for loading to finish
+                cy.get(testName('entity-name-container')).within(() => {
+                    cy.get('[data-test-is-loading="false"]')
+                })
+
+                cy.get(testName('add-description')).should('not.exist')
+
+                const descriptionUpdate = ' - With updates'
+                cy.get(testName('description-editor')).click()
+                cy.get(testName('description-editor')).focused().type(descriptionUpdate).blur()
+                cy.wait('@updateList').its('response.statusCode').should('equal', 200)
             })
 
             it('can add children', () => {
@@ -145,7 +174,7 @@ describe('Workspace', () => {
                 // @TODO: assert that task-tree has changed
             })
 
-            it('can edit the description', () => {
+            it('can add a description', () => {
                 const description = 'The testing entity description'
 
                 // wait for loading to finish
@@ -158,6 +187,35 @@ describe('Workspace', () => {
 
                 cy.get(testName('description-editor')).focused().type(description).blur()
                 cy.wait('@updateTask').its('response.statusCode').should('equal', 200)
+            })
+
+            it('can update the description', () => {
+                cy.get(testName('sidebar-create-new-list')).click()
+                cy.get(testName('editable-entity-name'))
+
+                // wait for loading to finish
+                cy.get(testName('entity-name-container')).within(() => {
+                    cy.get('[data-test-is-loading="false"]')
+                })
+
+                cy.get(testName('add-description')).click()
+                cy.get(testName('add-description')).should('not.exist')
+
+                const description = 'The testing entity description'
+                cy.get(testName('description-editor')).focused().type(description).blur()
+                cy.wait('@updateList').its('response.statusCode').should('equal', 200)
+
+                // wait for loading to finish
+                cy.get(testName('entity-name-container')).within(() => {
+                    cy.get('[data-test-is-loading="false"]')
+                })
+
+                cy.get(testName('add-description')).should('not.exist')
+
+                const descriptionUpdate = ' - With updates'
+                cy.get(testName('description-editor')).click()
+                cy.get(testName('description-editor')).focused().type(descriptionUpdate).blur()
+                cy.wait('@updateList').its('response.statusCode').should('equal', 200)
             })
 
             it('can add tasks', () => {
@@ -215,15 +273,41 @@ describe('Workspace', () => {
                     cy.get(testName('task-menu-button')).click()
                 })
 
-                // task menu
+                // task menu - create new subtask
                 cy.get(testName('drop-down-menu')).within(() => {
-                    cy.contains(/Subtask/)
+                    cy.contains(/Subtask/i)
                         .closest(testName('menu-item'))
                         .click()
                 })
 
                 cy.get(testName('task-tree-node')).should('have.length', 2)
                 cy.get(testName('task-tree-node')).last().should('have.attr', 'data-test-node-level', 1)
+            })
+
+            describe('Task description', () => {
+                it('can add a description', () => {
+                    cy.get(testName('task-tree-node')).within(() => {
+                        cy.get(testName('task-menu-button')).click()
+                    })
+
+                    // task menu - Add description
+                    cy.get(testName('drop-down-menu')).within(() => {
+                        cy.contains(/Description/i)
+                            .closest(testName('menu-item'))
+                            .click()
+                    })
+
+                    const descriptionCy = cy.get(testName('task-tree-node')).focused()
+                    descriptionCy.type('This is a description').blur()
+                    cy.wait('@updateTask').its('response.statusCode').should('equal', 200)
+                })
+
+                it.skip('can update a description', () => {
+                    // @TODO:
+                })
+                it.skip('can open the task as page from the description toolbar', () => {
+                    // @TODO:
+                })
             })
         })
     })
