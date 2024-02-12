8aebf1a40d7fc5a4c253856947498650987ed232
Floyd Haremsa <floyd.h@gmx.de>
2024-02-12 19:36:13 +0100

    chore: remove unused bash scripts

--- scripts/run-e2e.sh
+++
@@ -1,9 +0,0 @@
-db_url=$(bash ./sctipts/get-testing-db-url.sh)
-
-DATABASE_URL=$db_url npx nx run rockket-backend:db:reset
-
-echo ''
-echo '> Now running e2e tests'
-echo "> nx run $1"
-echo ''
-DATABASE_URL=$db_url nx run "$1"
