7f5c2e6b6e5cd520e03faad589ae74b017705d27
Floyd Haremsa <floyd.h@gmx.de>
2024-06-20 20:10:19 +0200

    Update apps/rockket-web/src/app/resize/resize-handle.component.spec.ts

--- apps/rockket-web/src/app/resize/resize-handle.component.spec.ts
+++ apps/rockket-web/src/app/resize/resize-handle.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { ResizeHandleComponent } from './resize-handle.component'
 import { ResizeDirective } from './resize.directive'
 
-describe('ResizeComponent', () => {
+describe(ResizeHandleComponent.name, () => {
     let component: ResizeHandleComponent
     let fixture: ComponentFixture<ResizeHandleComponent>
 
