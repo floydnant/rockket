1ec4bb17e708211dea42a6e34b1c94fea993c5b8
Floyd Haremsa <floyd.h@gmx.de>
2024-06-20 12:30:05 +0200

    feat: improve scrollbar styles (#608)

--- apps/rockket-web/src/app/components/templates/main-pane/main-pane.component.ts
+++ apps/rockket-web/src/app/components/templates/main-pane/main-pane.component.ts
@@ -18,9 +18,6 @@ import { MenuService } from '../sidebar-layout/menu.service'
                     @apply -translate-y-full;
                 }
             }
-            main {
-                @apply not-hover:[&_::-webkit-scrollbar-thumb]:bg-transparent;
-            }
         `,
     ],
     changeDetection: ChangeDetectionStrategy.OnPush,
